92683793bfede825e6b311282ae63b4a8d616fff
Lerroy Gatimu <lerroygatimu@gmail.com>
2025-09-08 12:24:45 +0300

    2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,8 +1,30 @@
-argument = 1
+argument = 0
 if (argument < 1){
   console.log("No argument")
 }  
 else if (argument > 0){
+  console.log ("Argument found")
+}  
+else{
+  console.log("Arguments found")
+}  
+
+argumen = 1
+if (argumen < 1){
+  console.log("No argument")
+}  
+else if (argumen > 0){
+  console.log ("Argument found")
+}  
+else{
+  console.log("Arguments found")
+}  
+
+argume = 3
+if (argume < 1){
+  console.log("No argument")
+}  
+else if (argume > 0){
   console.log ("Argument found")
 }  
 else{
